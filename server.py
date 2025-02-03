f40c5ff30d81d894897b866ee85ad0d81de4cd2e
sins621 <bradlycarpenterza@gmail.com>
2025-02-03 17:00:16 +0200

    update/cleaned-prototypes

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 import base64
-import json
 
 import markdown
 import markdown.extensions.fenced_code
@@ -10,37 +9,12 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 cors = CORS(app)
 app.config["CORS_HEADERS"] = "Content-Type"
-git_url = "https://github.com/sins621/Obsidian-Notes"
-to_path = "./github-files"
-
-github_user = "sins621"
-github_repo = "Obsidian-Notes"
-dir_path = "Programming/CPP/Controlling Program Flow"
-file_path = "Looping.md"
-
-github_file_api_url = f"https://api.github.com/repos/{github_user}/{github_repo}/contents/{dir_path}/{file_path}"
-github_tree_api_url = f"https://api.github.com/repos/{github_user}/{github_repo}/git/trees/main?recursive=1"
-# https://api.github.com/repos/sins621/Obsidian-Notes/git/trees/main?recursive=1
-response = requests.get(url=github_tree_api_url)
-data = response.json()
-content = data["tree"]
-filter_string = ".md"
-filtered_data = []
-
-for dict in content:
-    if filter_string in dict["path"]:
-        filtered_data.append(dict)
-# print(json.dumps(filtered_data, indent=2))
-
-for dict in filtered_data:
-    print(dict["path"].rpartition("/")[0])
 
 
-@app.route("/get_tree/")
+@app.route("/get_tree")
 @cross_origin()
 def get_tree():
     github_tree_api_url = f"https://api.github.com/repos/{request.args.get("github_user")}/{request.args.get("github_repo")}/git/trees/main?recursive=1"
-    # https://api.github.com/repos/sins621/Obsidian-Notes/git/trees/main?recursive=1
     response = requests.get(url=github_tree_api_url)
     data = response.json()
     content = data["tree"]
@@ -59,6 +33,7 @@ def get_tree():
 
 
 @app.route("/get_file")
+@cross_origin()
 def get_file():
     args = request.args
     response = requests.get(url=args.get("url"))
@@ -76,24 +51,3 @@ def get_file():
 
 if __name__ == "__main__":
     app.run(debug=True, port=3000)
-
-
-# content = data["content"]
-# decoded_string = base64.b64decode(content).decode("utf-8")
-
-# decoded_string = decoded_string.replace(
-#     "![](",
-#     f"![](https://raw.githubusercontent.com/{github_user}/{github_repo}/refs/heads/main/{dir_path}/",
-# )
-
-# html = markdown.markdown(decoded_string, extensions=["fenced_code"])
-
-
-# @app.route("/")
-# @cross_origin()
-# def hello_world():
-#     return html
-
-
-# if __name__ == "__main__":
-#     app.run(debug=True, port=3000)
