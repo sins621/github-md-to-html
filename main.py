52df61cc9a3f6b024fb12b9a714b2ff456f08c44
sins621 <bradlycarpenterza@gmail.com>
2025-02-09 22:28:59 +0200

    update/changed-to-helper-functions

--- main.py
+++ main.py
@@ -3,29 +3,15 @@ import base64
 import markdown
 import markdown.extensions.fenced_code
 import requests
-from flask import Flask, jsonify, request
-from flask_cors import CORS, cross_origin
-
-app = Flask(__name__)
-cors = CORS(app)
-app.config["CORS_HEADERS"] = "Content-Type"
 
 
-# TODO: Add try catch and validate args
-
-
-@app.route("/get_tree")
-@cross_origin()
-def get_tree():
-    print(request.args.get("github_user"))
-    print(request.args.get("github_repo"))
-    github_tree_api_url = f"https://api.github.com/repos/{request.args.get("github_user")}/{request.args.get("github_repo")}/git/trees/main?recursive=1"
+def get_tree(github_user, github_repo):
+    github_tree_api_url = f"https://api.github.com/repos/{github_user}/{github_repo}/git/trees/main?recursive=1"
     response = requests.get(url=github_tree_api_url)
     data = response.json()
     content = data["tree"]
     filter_string = ".md"
     filtered_data = []
-
     for dict in content:
         if filter_string in dict["path"]:
             filtered_data.append(
@@ -34,23 +20,16 @@ def get_tree():
                     "url": dict["url"],
                 }
             )
+    return filtered_data
 
-    return jsonify(filtered_data)
 
-
-@app.route("/get_file")
-@cross_origin()
-def get_file():
-    args = request.args
-    github_user = args.get("github_user")
-    github_repo = args.get("github_repo")
-    file_url = args.get("url", None)
+def get_file(github_user, github_repo, file_url, path):
     if file_url:
         response = requests.get(url=file_url)
         response.raise_for_status()
         data = response.json()
         content = data["content"]
-        dir = args.get("path", "").rpartition("/")[0]
+        dir = path.rpartition("/")[0]
         decoded_string = base64.b64decode(content).decode("utf-8")
         decoded_string = decoded_string.replace(
             "![](",
@@ -58,7 +37,3 @@ def get_file():
         )
         html = markdown.markdown(decoded_string, extensions=["fenced_code"])
         return html
-
-
-if __name__ == "__main__":
-    app.run(debug=True, port=3000)
